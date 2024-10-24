f143544c41c4bdb51988fa15ca186913b61bd080
EYDER DE JESUS ARROYO VEGA <eyderarroyo19@gamil.com>
2024-10-23 23:07:28 -0500

    first commit

---
+++ HolaMundo.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class HolaMundo {
+    Run|Debug
+public static void main(String] args) {
+// Imprimir datos en pantalla consola / terminal
+System.out.println(x:10);
+System.out.print(s: "Como van las cosas");
+System.out.print(1:10);
+System.out.print(d:10.5);
+System.out.print(c: 'A');
+System.out.println();
+
+
+
+System.out.printf(format:"Aqui va una cadena: %s\n", args: "Cesar");
+System.out.printf(format:"Aqui va un entero: %d\n",...args:10);
+System.out.printf(format: "Mi nombre es %s y tengo %d años%n",...args: "Cesar Diaz", 15);
+
+var entrada new Scanner(System.in);
+System.out.print(s: "Por favor ingrese su nombre: ");
+var entero = entrada.nextLine();
+
+System.out.println(entero);
+entrada.close();	
+        //
+    }
+
+}
